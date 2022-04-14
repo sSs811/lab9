72a18d0e54dedae0a84f8c619812ab407e9f30fe
Lera <78473376+sSs811@users.noreply.github.com>
2022-04-14 23:20:57 +0700

    Add files via upload

---
+++ CheckImage/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckImage.ViewModels
+{
+    internal class MainWindowViewModel
+    {
+        private FilesPageViewModel? _files;
+
+        public FilesPageViewModel Files
+        {
+            get => _files ??= new FilesPageViewModel();
+        }
+    }
+}
